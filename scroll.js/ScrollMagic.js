3d092de76dd172457dd814d43b876acc8909ae43
Peter Kimanzi <71552773+peter-kimanzi@users.noreply.github.com>
2022-04-15 19:35:33 +0300

    Update ScrollMagic.js

--- scroll.js/ScrollMagic.js
+++ scroll.js/ScrollMagic.js
@@ -1,4 +1,18 @@
-
+/*!
+ * ScrollMagic v2.0.8 (2020-08-14)
+ * The javascript library for magical scroll interactions.
+ * (c) 2020 Jan Paepke (@janpaepke)
+ * Project Website: http://scrollmagic.io
+ * 
+ * @version 2.0.8
+ * @license Dual licensed under MIT license and GPL.
+ * @author Jan Paepke - e-mail@janpaepke.de
+ *
+ * @file ScrollMagic main library.
+ */
+/**
+ * @namespace ScrollMagic
+ */
 (function (root, factory) {
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
